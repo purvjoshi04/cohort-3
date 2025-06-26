b95b9e5ef3e5b215322a95f6d2ecd2330c354d21
Purv Joshi <purvadjoshi@gmail.com>
2025-06-26 22:49:10 +0530

    promise, date and map functions in js

--- week-2_js_advance/2.2_promises/index.js
+++ week-2_js_advance/2.2_promises/index.js
@@ -17,3 +17,23 @@ class Rectangle {
 const rect = new Rectangle(3, 4, "red");
 rect.area();
 rect.paint();
+
+
+// map function
+const map = new Map();
+map.set("name", "purv");
+map.set("age", 21);
+console.log(map.get("name"));
+
+
+// promise
+
+function setTimeoutPromisified(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function callback() {
+  console.log("3 seconds have passed!");
+}
+
+setTimeoutPromisified(3000).then(callback)
