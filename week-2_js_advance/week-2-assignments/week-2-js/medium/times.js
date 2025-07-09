cb61388d5771121a03fc7a176935bdcfc6ea8b0f
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2025-07-09 21:53:11 +0530

    js assignments

--- week-2_js_advance/week-2-assignments/week-2-js/medium/times.js
+++ week-2_js_advance/week-2-assignments/week-2-js/medium/times.js
@@ -8,6 +8,18 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime(n) {
-    return 0.01;
+function calculateTime(n) {          
+    const startTime = performance.now(); 
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    const endTime = performance.now();
+
+    return (endTime - startTime) / 1000;
 }
+
+console.log('Time for sum from 1 to 100:', calculateTime(100));
+console.log('Time for sum from 1 to 100000:', calculateTime(100000));
+console.log('Time for sum from 1 to 1000000000:', calculateTime(1000000000));
