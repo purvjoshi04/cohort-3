eec4e59e189a6eeb9ac39e2cbf17ec5c21e6c1b6
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2025-07-20 15:23:39 +0530

    map and filer function in JS

---
+++ week-5/5.4_map_filter/index.js
@@ -0,0 +1,10 @@
+// map function in JS
+const arr = [1, 2, 3, 4, 5];
+
+const transformedArr = arr.map((n) => n * 2);
+console.log(transformedArr);
+
+// filter function in JS
+
+const evenNum = arr.filter((n) => n % 2 === 0);
+console.log(evenNum);
