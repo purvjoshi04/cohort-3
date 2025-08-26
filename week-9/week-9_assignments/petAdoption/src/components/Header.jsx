32193c52f53d35d5f12f166bdfac87adaadffba0
purvjoshi04 <101319136+purvjoshi04@users.noreply.github.com>
2025-08-26 23:25:28 +0530

    week-9 assignments

---
+++ week-9/week-9_assignments/petAdoption/src/components/Header.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+const Header = ({message}) => {
+  return (
+    <div style={{backgroundColor: "#c59771bd", padding: "16px 32px"}}>
+      {message}
+    </div>
+  )
+}
+
+export default Header
